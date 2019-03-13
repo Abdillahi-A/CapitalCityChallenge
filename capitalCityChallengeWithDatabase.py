f9e2c07b782b9ea23190191438cc88e74b2c2380
Abdillahi Abdillahi <Abdillahi@Abdillahis-MacBook-Air.local>
2019-03-13 20:21:49 +0000

    Added database to game

---
+++ capitalCityChallengeWithDatabase.py
@@ -0,0 +1,117 @@
+from continents import conts
+import sys
+import random
+from fuzzywuzzy import fuzz
+import sqlite3
+
+
+def getUserInput():
+    name = input("Please choose a username: ")
+    return name
+
+def leave_game():
+    print("'\nThat's too bad. Come back when you want to play again.\nBye!")
+    sys.exit()
+
+def choose_continent():
+        continent = input("Choose a continent: \n{}\n\n".format('\n'.join(conts)))
+        return continent
+        
+def check_valid_continent(continent):
+    while continent.capitalize() not in conts.keys():
+        continent = input("Sorry that is not a valid response. Please choose one of the follwoing continents: \n{}\n\n".format('\n'.join(conts.keys())))
+        
+    return continent
+    
+def randomise_countries(continent):
+    items = list(conts[continent.capitalize()].items())
+    random.shuffle(items)
+    return items
+
+def ask_question(continent):
+    correct_answers = 0
+    total_questions = len(conts[continent.capitalize()])
+    items = randomise_countries(continent)
+       
+    for country,city in items:
+        answer = input('What is the capital city of {}\n'.format(str(country)))
+        if check_answer(answer, city) == True:
+            correct_answers += 1
+    
+    return correct_answers, total_questions
+          
+def check_answer(answer, city):
+    if fuzz.ratio(answer.lower(), city.lower()) != 100 and fuzz.ratio(answer.lower(), city.lower()) > 90:
+        print(f"Hmmm I'll give you this one, but the correct spelling is {city}.\n")
+        return True
+    elif fuzz.ratio(answer.lower(), city.lower()) == 100:
+        print("Well done! That is correct.\n")
+        return True
+    else:
+        print("Sorry, the correct answer is {}\n".format(city))
+
+def display_score(correct_answers, total_questions):
+    print(f" You got {correct_answers} out of cities {total_questions} correct.")
+
+def score_the_game(correct_answers, total_questions):
+    print('\nResult:')
+    if correct_answers / total_questions == 1:
+        display_score(correct_answers, total_questions)
+        print('Wow, you really know your stuff. Very Impressed!')
+    elif correct_answers / total_questions > 0.7:
+        display_score(correct_answers, total_questions)
+        print('Quite the geographer we have here. I am impressed')
+    elif correct_answers / total_questions > 0.5:
+        display_score(correct_answers, total_questions)
+        print('Not bad, not bad at all.')
+    elif correct_answers / total_questions > 0.4:
+        display_score(correct_answers, total_questions)
+        print('Admirable attemp young Grasshopper. Though there is yet room for improvement.')
+    else:
+        display_score(correct_answers, total_questions)
+        print('Looks like you need a little more practice.') 
+
+def play_again():
+    play_again = input("Do you want to play again? Please enter 'Y' or 'N'\n")
+    
+    while play_again.lower() not in ['y', 'n']:
+        play_again = input("Please type 'Y' to continue or 'N' to quit\n")
+
+    if play_again.lower() == 'y':
+        return True
+
+def display_high_scores(name, correct_answers, conn):
+    c = conn.cursor()
+    c.execute("INSERT INTO highScores (username, highscore) VALUES (?,?)", (name, correct_answers))
+    conn.commit()
+
+    print('\n')
+    print("HighScores:")
+
+    for row in c.execute('SELECT username, highscore FROM highScores ORDER BY highscore desc LIMIT 5'):
+        print('{:15} | {}'.format(row[0], row[1]))
+
+    
+def main():
+    conn = sqlite3.connect('highScores.db')
+    c = conn.cursor()
+    c.execute('''CREATE TABLE IF NOT EXISTS highScores
+             (username text, highscore int)''')
+    conn.commit()
+
+
+
+    print("Hey, Welcome to Capital City Challenge!\n")
+    while True:  
+        name = getUserInput()
+        continent = choose_continent()
+        continent = check_valid_continent(continent)
+        print("Cool. You've Chosen {}. Let's see if you know your stuff.".format(continent.capitalize()))
+        correct_answers, total_questions = ask_question(continent)
+        score_the_game(correct_answers, total_questions)
+        display_high_scores(name, correct_answers, conn)
+        if not play_again():
+            leave_game()
+            break
+            
+main()
